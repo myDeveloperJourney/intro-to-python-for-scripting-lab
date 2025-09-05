4d9ada159624a36a046d49c5f4f04611b3b4c159
myDeveloperJourney <admin@mdjstudios.com>
2025-09-05 12:50:13 -0500

    completes ex 1

---
+++ hello_python.py
@@ -0,0 +1,6 @@
+# Write a Python script that prompts the user for their name.
+first_name = input("Please enter your first name: ")
+# The script should also greet them with a personalized message containing their name.
+print(f"Hello, {first_name}! It's nice to meet you.")
+
+# Run the script to confirm it does what you want it to!
